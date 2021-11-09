10f1805abc5a357536e1e0665453c28e24b954d9
zakarea12345 <saiakzakarea7@gmail.com>
2021-11-09 16:08:00 +0600

    modal displayed

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -3,10 +3,15 @@ import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import BookingModal from '../BookingModal/BookingModal';
 
 const Booking = ({booking}) => {
     const {name, time, space} = booking;
+    const [openBooking, setOpenBooking] = React.useState(false);
+    const handleBookingOpen = () => setOpenBooking(true);
+    const handleBookingClose = () => setOpenBooking(false);
     return (
+        <>
         <Grid item xs={12} sm={6} md={4}>
            <Paper elevation={3} sx={{py:5}}>
               <Typography variant="h5" gutterBottom component="div" sx={{ color: 'info.main' , fontWeight:600 }}>
@@ -18,9 +23,15 @@ const Booking = ({booking}) => {
               <Typography variant="caption" gutterBottom component="div">
                   {space} Spaces Available
               </Typography>
-              <Button variant="contained">BOOK APPOINTMENT</Button>
+              <Button onClick={handleBookingOpen} variant="contained">BOOK APPOINTMENT</Button>
            </Paper>    
         </Grid>
+        <BookingModal
+          handleBookingClose={handleBookingClose}
+          openBooking={openBooking}
+          booking={booking}
+        ></BookingModal>
+        </>
     );
 };
 
